700f8bef894551b28b6b508148ae7c0cd9d79d52
Mel Huang <hello@melaniehuang.com>
2025-05-15 23:13:42 +1000

    fixing crossorogin

--- src/index.tsx
+++ src/index.tsx
@@ -73,7 +73,7 @@ app.get("/", async (c) => {
       <head>
         <link href="/public/style.css" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Michroma&display=swap" rel="stylesheet"></link>
         <script src="/public/js/accordion.js" />
       </head>
